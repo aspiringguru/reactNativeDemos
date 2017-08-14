0fd80442fd5428d2d690289c285cf5a5696a8b75
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-14 19:50:52 +1000

    Awesome Project - heigh & width - justify content

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -1,28 +1,30 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
+ * https://facebook.github.io/react-native/docs/flexbox.html#content
  */
 
  import React, { Component } from 'react';
  import { AppRegistry, View } from 'react-native';
 
- export default class FlexDimensionsBasics extends Component {
+ export default class JustifyContentBasics extends Component {
    render() {
      return (
-       // Try removing the `flex: 1` on the parent View.
-       // The parent will not have dimensions, so the children can't expand.
-       // What if you add `height: 300` instead of `flex: 1`?
-       <View style={{flex: 1}}>
-         <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-         <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-         <View style={{flex: 4, backgroundColor: 'red'}} />
-         <View style={{flex: 10, backgroundColor: 'yellow'}} />
+       // Try setting `justifyContent` to `center`. space-between
+       // Try setting `flexDirection` to `row`.
+       <View style={{
+         flex: 1,
+         flexDirection: 'column',
+         justifyContent: 'center',
+       }}>
+         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+         <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
        </View>
      );
    }
- }
+ };
 
  // skip this line if using Create React Native App
- AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
+ AppRegistry.registerComponent('AwesomeProject', () => JustifyContentBasics);
