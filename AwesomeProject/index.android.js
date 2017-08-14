96ed20c7be51d46b68d78635bd099c5fc7d769a0
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-14 18:48:30 +1000

    Awesome Project - props

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -8,14 +8,30 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   Text,
+  View
 } from 'react-native';
 
+class Greeting extends Component {
+  render() {
+    return ( <Text>Hello {this.props.name}! </Text> );
+  }//end render()
+}//end class Greeting extends Component
+
 export default class AwesomeProject extends Component {
+//export default class LotsOfGreetings extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+        <Greeting name='Matthew' />
+        <Greeting name='Mark' />
+        <Greeting name='Luke' />
+      </View>
     );
   }
 }
+
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
