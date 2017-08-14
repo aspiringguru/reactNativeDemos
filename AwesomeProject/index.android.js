3b76a6ac7d1d3b90cd9ae42f0104d6f1a28ee3b3
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-14 19:29:53 +1000

    Awesome Project - heigh & width - fixed

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -4,37 +4,21 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, { Component } from 'react';
+ import { AppRegistry, View } from 'react-native';
 
-export default class LotsOfStyles extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigblue}>just bigblue</Text>
-        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
-      </View>
-    );
-  }
-}
+ export default class FixedDimensionsBasics extends Component {
+   render() {
+     return (
+       <View>
+         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+         <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+         <View style={{width: 100, height: 100, backgroundColor: 'red'}} />
+       </View>
+     );
+   }
+ }
 
-const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
-
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+ // skip this line if using Create React Native App
+ AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
