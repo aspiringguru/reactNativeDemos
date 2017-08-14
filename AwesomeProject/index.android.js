288c701645b29f34a67d7daea287771e43afae0a
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-14 20:21:31 +1000

    Awesome Project - heigh & width - text input pizza emoji

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -5,26 +5,35 @@
  */
 
  import React, { Component } from 'react';
- import { AppRegistry, View } from 'react-native';
+ import { AppRegistry, Text, TextInput, View } from 'react-native';
 
- export default class JustifyContentBasics extends Component {
+ export default class PizzaTranslator extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {text: ''};
+   }
+
    render() {
      return (
-       // Try setting `justifyContent` to `center`. space-between
-       // Try setting `flexDirection` to `row`.
-       <View style={{
-         flex: 1,
-         flexDirection: 'column',
-         justifyContent: 'center',
-       }}>
-         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-         <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+       <View style={{padding: 10}}>
+         <TextInput
+           style={{height: 40}}
+           placeholder="Type here to translate!"
+           onChangeText={(text) => this.setState({text})}
+         />
+         <Text style={{padding: 10, fontSize: 42}}>
+           {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+         </Text>
+         <Text style={{padding: 10, fontSize: 42}}>
+           {this.state.text.split(' ')[0]}
+         </Text>
+         <Text style={{padding: 10, fontSize: 42}}>
+           {this.state.text.split(' ')[1]}
+         </Text>
        </View>
      );
    }
- };
+ }
 
  // skip this line if using Create React Native App
- AppRegistry.registerComponent('AwesomeProject', () => JustifyContentBasics);
+ AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
